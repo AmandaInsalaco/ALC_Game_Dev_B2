277ffdad98525da86965512d6d391752f291cc65
Unknown <mandee.insalaco@gmail.com>
2018-10-02 10:28:42 -0600

    Created Particle System for Death and Respawn

--- 2D Game/Assets/Scripts/LevelManager.cs
+++ 2D Game/Assets/Scripts/LevelManager.cs
@@ -32,14 +32,18 @@ public class LevelManager : MonoBehaviour {
 
     public IEnumerable RespawnPlayerCo(){
         //generate death particle
+        //instantiate is creating a game object in our world
         //first part is game object we want to create
         //next spot is where we want it to be created
         //third spot is transformation and rotation
         Instantiate(deathParticle, PC.transform.position, PC.transform.rotation);
         //Hide Player
-        PC.enabled = false;
+        //use this as a smoke and mirrors effect while we move the player
+        //PC.enabled = false;
         PC.GetComponent<Renderer>().enabled = false;
         //gravity Reset
+        //with rigidy body there's velocity, a PC has the same 
+        //velocity that it had when it dies, so we need to reset
         gravityStore = PC.GetComponent<Rigidbody2D>().gravityScale;
         PC.GetComponent<Rigidbody2D>().gravityScale = 0f;
         PC.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
@@ -48,12 +52,17 @@ public class LevelManager : MonoBehaviour {
         //debug message
         Debug.Log("Player Respawn");
         //respawn delay
+        //pause so that you don't insatntly reappear
         yield return new WaitForSeconds(respawnDely);
         //Gravity Restore
+        //took away the gravity and now we are restoring it
         PC.GetComponent<Rigidbody2D>().gravityScale = gravityStore;
         //match Players transform  positions
+        //puts the player at the check point
         PC.transform.position = currentCheckPoint.transform.position;
         //Show Player
-
+        //PC.enabled = true;
+        PC.GetComponent<Renderer>().enabled = true;
+        Instantiate(respawnParticle, currentCheckPoint.transform.position, currentCheckPoint.transform.rotation);
     }
 }
