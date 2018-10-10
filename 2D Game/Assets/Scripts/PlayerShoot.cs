87b8ff4e0db5cf9717da19e47b51d03dc3510f6e
Unknown <mandee.insalaco@gmail.com>
2018-10-10 10:29:32 -0600

    Added a Projectile

---
+++ 2D Game/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+    public Transform firePoint;
+    public GameObject projectile;
+	
+	// Update is called once per frame
+    // the projectile inherients the firepoints position and orientation
+    //.S could be .RightControl
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.S))
+            Instantiate(projectile,firePoint.position, firePoint.rotation);
+	}
+}
---
+++ 2D Game/Assets/Scripts/Projectile.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float Speed;
+    public Rigidbody2D PC;
+    public GameObject EnemyDeath;
+    public GameObject ProjectileParticle;
+    public int PointsForKill;
+
+    // Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, GetComponent<Rigidbody2D>().velocity.y);
+        //PC = FindObjectOfType<RigidBody2D>();
+        //shoots in the direction the PC is facing
+        //do not need braces if you are only using one line
+        if (PC.transform.localScale.x < 0)
+            Speed = -Speed;
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        //kills enemy when projectile hits enemy
+        //adds points
+        if(other.tag == "Enemy"){
+            Instantiate(EnemyDeath, other.transform.position, other.transform.rotation);
+            Destroy(other.gameObject);
+            ScoreManager.AddPoints(PointsForKill);
+            Debug.Log("Projectile Hit");
+        }
+        //the projectile tells player that we hit the enemy
+        //is instantiated right where the projectile hits
+        //destroys projectile
+        //When we destory a game object, it no longer exsists, the collider, and script is gone too.
+        Instantiate(ProjectileParticle, transform.position, transform.rotation);
+        Destroy(gameObject);
+	}
+}
