7b84c78222995dab1a932fb0b61a0b712ea03c6c
Unknown <mandee.insalaco@gmail.com>
2018-10-30 10:29:11 -0600

    Added CameraFollow

---
+++ 2D Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public PC_CharMove Player;
+    public bool isFollowing;
+    //camera position offset
+    public float xoffset;
+    public float yoffset;
+
+    // Use this for initialization
+	void Start () {
+        Player = FindObjectOfType<PC_CharMove>();
+        isFollowing = true;
+	}
+	
+	// Update is called once per frame
+    //z is mostly used for depth, to zoom in and out
+	void Update () {
+        if (isFollowing)
+        {
+            transform.position = new Vector3(Player.transform.position.x + xoffset, Player.transform.position.y + yoffset, transform.position.z);
+        }
+        }
+}
