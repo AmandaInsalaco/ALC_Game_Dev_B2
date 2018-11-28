598c9433f1dc65b1223ef7dfe5abcb4500ab24d7
Unknown <mandee.insalaco@gmail.com>
2018-11-28 10:29:47 -0700

    Added Main Menu

---
+++ 2D Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public int levelToLoad;
+
+    //Load specific Level
+    public void LoadLevel(){
+        SceneManager.LoadScene(levelToLoad);
+    }
+    //Exits out of the game
+    public void LevelExit(){
+        Application.Quit();
+    }
+}
