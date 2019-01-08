081740269e2664fe15ce09f54705ad14830e54b2
Unknown <mandee.insalaco@gmail.com>
2019-01-08 10:22:56 -0700

    Added WIN screen and LevelControl

---
+++ 2D Game/Assets/Scripts/LevelControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour {
+
+    public int index;
+    public string levelName;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.CompareTag("Player"))
+        {
+            //loading level with build index
+            SceneManager.LoadScene(index);
+
+            //loading level with scene name
+            SceneManager.LoadScene(levelName);
+
+            //Restart lvl1
+            //SceneManger.LoadScene(SceneManger.GetActiveScene().buildIndex);
+        }
+	}
+}
